d4ef21964545eb2d6c09a62076c997495adfdc5d
Sebastian Rivera Gonzalez <sebas.rivera96@gmail.com>
2018-11-15 18:38:06 -0600

    Solve issues when loading & saving data to JSON file
    At this moment, data can be successfully stored to the JSON file
    and read from it.

--- Face_Recognition_Functions.py
+++ Face_Recognition_Functions.py
@@ -1,21 +1,25 @@
 import face_recognition
+import numpy as np
 from PIL import Image
+import json
 import sys
 import os
 
 # =========================== GLOBAL VARIABLES =============================== #
 knownPeople = {} # Dictionary: {key = Name, value = encoding}
+JSONPath = "known_People.json"
+# ============================================================================= #
 
 # ============================= MAIN FUNTIONS ================================ #
 """
-Function Name:
-    facesOnImg
-Objective:
-    Return a list the of faces on the given image object.
-Input parameter(s):
-    - imgObj: An image object loaded from with the face_recognition.load_image_file function
-Return value(s):
-    - faceLocations: A list of tuples of found face locations in css (top, right, bottom, left) order.
+    Function Name:
+        facesOnImg
+    Objective:
+        Return a list the of faces on the given image object.
+    Input parameter(s):
+        - imgObj: An image object loaded from with the face_recognition.load_image_file function
+    Return value(s):
+        - faceLocations: A list of tuples of found face locations in css (top, right, bottom, left) order.
 """
 def facesOnImg(imgObj):
     # Use a built-in function from the library to detect faces
@@ -23,18 +27,18 @@ def facesOnImg(imgObj):
     return faceLocations
 
 """
-TODO
-Function Name:
-    identifyKnownPeople
-Objective:
-    Load an image with the face_recognition library and search for a known 
-    person using the encodings that were previously generated and stored.
-Input parameter(s):
-    - imgPath : A relative path to the image. Preferably consider to have 
-        a local director to the repository to store the images.
-    - facesEncodings : TODO define wher this encodings will be stored, e.g.
-        on a txt file, on a directory created during the runtime.
-Return value(s):
+    TODO
+    Function Name:
+        identifyKnownPeople
+    Objective:
+        Load an image with the face_recognition library and search for a known 
+        person using the encodings that were previously generated and stored.
+    Input parameter(s):
+        - imgPath : A relative path to the image. Preferably consider to have 
+            a local director to the repository to store the images.
+        - facesEncodings : TODO define wher this encodings will be stored, e.g.
+            on a txt file, on a directory created during the runtime.
+    Return value(s):
     - recognizedPeople : A list containig the names of the recognized people.
 """
 def identifyKnownPeople(imgPath, facesEncodings):
@@ -51,27 +55,30 @@ def identifyKnownPeople(imgPath, facesEncodings):
     return recognizedPeople
 
 """
-TODO
-Function Name:
-    learnOnNewFace
-Objective:
-    An image with a single face will be received to learn on a new face. If the
-    number of detected faces on the img is not 1, an error will be returned.
-Input parameter(s):
-    - imgPath : A relative path to the image.
-    - nameOfPerson : A string that represents the name of the new person.
-    - facesDict : Dictionary with key NAME : value FACE_ENCODING.
-Return value(s): 
-    - successState : Integer value communicating success or failure
-        * 0 : The face was correctly learned
-        * -1 : There was a problem and the new face WASN'T learned, e.g. the 
-        number of faces detected on the image were 0 or more than 1
+    Function Name:
+        learnOnNewFace
+    Objective:
+        An image with a single face will be received to learn on a new face. If the
+        number of detected faces on the img is not 1, an error will be returned.
+    Input parameter(s):
+        - imgPath : A relative path to the image.
+        - nameOfPerson : A string that represents the name of the new person.
+        - facesDict : Dictionary with key NAME : value FACE_ENCODING.
+    Return value(s): 
+        - successState : Integer value communicating success or failure
+            * 0 : The face was correctly learned OR nameOfPerson is already registered
+            * -1 : There was a problem and the new face WASN'T learned, e.g. the 
+            number of faces detected on the image were 0 or more than 1
 """
 def learnOnNewFace(imgPath, nameOfPerson):
     global knownPeople
-    imgObj = face_recognition.load_image_file(imgPath)
+    # 0) If nameOfPerson already exists, exit func. returning 0
+    if nameOfPerson in knownPeople.keys():
+        print("\n !!!!! Face already registered !!!!! \n")
+        return 0
 
     # 1) Find faces on the image
+    imgObj = face_recognition.load_image_file(imgPath)
     faceLocation = facesOnImg(imgObj)
 
     # 2) If not 1, return -1 : else, keep going
@@ -80,69 +87,101 @@ def learnOnNewFace(imgPath, nameOfPerson):
 
     # 3) Store the new encoding (value) related to nameOfPerson (key)
     newEncoding = face_recognition.face_encodings(imgObj, faceLocation)[0]
-    knownPeople[nameOfPerson] = newEncoding
+
+    # 4) Convert numpy.ndarray ==> list
+    knownPeople[nameOfPerson] = newEncoding.tolist()
 
     # 4) Return a success state
     return 0
 
 """
-TODO
-Function Name:
-    areTheySameFace
-Objective:
-    Compare two face's encodings and return a boolean determining if they are 
-    the same person.
-Input parameter(s): Both of type 'numpy.ndarray'
-    - encodingOne 
-    - encodingTwo
-Output parameter(s):
-    - Boolean 'True' if there is a match; else 'False'.
+    Function Name:
+        areTheySameFace
+    Objective:
+        Compare two face's encodings and return a boolean determining if they are 
+        the same person.
+    Input parameter(s): Both lists, which must be processed before comparing
+        - encodingOne 
+        - encodingTwo
+    Output parameter(s):
+        - Boolean 'True' if there is a match; else 'False'.
 """
 def areTheySameFace(encodingOne, encodingTwo):
+    # 1) Convert from type list to np.array
+    encodingOne = np.array(encodingOne)
+    encodingTwo = np.array(encodingTwo)
+
+    # 2) First parameter must be a list of np.array and second parameter must
+    # be a single np.array
     returnValue = face_recognition.compare_faces([encodingOne], encodingTwo)
-    return returnValue[0]
-
-"""
-TODO
-Function Name:
-    loadKnownFaces
-Objective:
-    Load the dictonary that contains the information about the information 
-    from the already learned faces.
-Input parameter(s):
-    - jsonPath : A string of the relative path to the JSON file.
-Return value(s):
-    - facesDict : Dictionary with key NAME : value FACE_ENCODING.
-"""
-# def loadKnownFaces(jsonPath):
-
-
-"""
-TODO
-Function Name:
-    saveNewFaces
-Objective:
-
-Input parameter(s):
-
-Output parameter(s):
-
-"""
-# def saveNewFaces(facesDict):
-
-"""
-TODO
-Function Name:
     
-Objective:
+    # 3) returnValue is a one-element list, thus return element 0
+    return(returnValue[0])
 
-Input parameter(s):
+"""
+    Function Name:
+        loadKnownFaces
+    Objective:
+        Load the dictonary that contains the information about the information from the already learned faces.
+    Input parameter(s):
+        - jsonPath : A string of the relative path to the JSON file.
+    Return value(s):
+        * None because the data is loaded in a global dictionary called knownPeople
+"""
+def loadKnownFaces(jsonPath):
+    global knownPeople
+    with open("known_People.json", 'r') as read_file:
+        knownPeople = json.load(read_file)  
 
-Output parameter(s):
+"""
+    Function Name:
+        saveNewFaces
+    Objective:
 
+    Input parameter(s):
+        * None
+    Output parameter(s):
+        * None
+"""
+def saveNewFaces():
+    global knownPeople
+    with open("known_People.json", 'w') as write_file:
+        json.dump(knownPeople, write_file)
+
+    print("\n===== Saved knownPeople in JSON file =====\n")
+
+"""
+    Function Name:
+        printKnownPeople
+    Objective:
+        Print data stored on global dictionary 'knownPeople'.
+    Input parameter(s):
+        * None
+    Output parameter(s):
+        * None
+"""
+def printKnownPeople():
+    global knownPeople
+    print("\n")
+    for k,v in knownPeople.items():
+        print("-------------------------------------------")
+        print(k, "==>", v[0:2], " ... ", v[-2:])
+    print("-------------------------------------------\n")
+
+"""
+    TODO
+    Function Name:
+        
+    Objective:
+
+    Input parameter(s):
+
+    Output parameter(s):
 """
 
 if __name__ == "__main__":
+    loadKnownFaces("known_People.json")
+
     imgPath = "Leo_Messi/3.jpg"
     imgPath_CR7 = "Cristiano_Ronaldo/1.jpg"
     imgPath_GB = "Gareth_Bale/1.jpg"
@@ -151,27 +190,21 @@ if __name__ == "__main__":
     print("Register Cristiano: ", learnOnNewFace(imgPath_CR7, "Cristiano Ronaldo"))
     print("Register Gareth: ", learnOnNewFace(imgPath_GB, "Gareth Bale"))
     
-    imgPath = "Gareth_Bale/3.jpg"
-    imgObject = face_recognition.load_image_file(imgPath)
-    faceEncoding = face_recognition.face_encodings(imgObject)[0]
-
-    for key, value in knownPeople.items():
-        if areTheySameFace(value, faceEncoding):
-            print(key)
-
-    # imgObj_CR7 = face_recognition.load_image_file(imgPath_CR7)
-    # imgObj_GB = face_recognition.load_image_file(imgPath_GB)
+    # imgPath = "Gareth_Bale/3.jpg"
+    # imgObject = face_recognition.load_image_file(imgPath)
+    # faceEncoding = face_recognition.face_encodings(imgObject)
+    # newFaceEncoding = faceEncoding[0].tolist()
 
 
-    # faceEncoding_CR7 = face_recognition.face_encodings(imgObj_CR7)[0]
-    # faceEncoding_GB = face_recognition.face_encodings(imgObj_GB)[0]
+    # knownPeople["Gareth Bale"] = newFaceEncoding
+    # for key, value in knownPeople.items():
+    #     print(value, type(value))
+        # if areTheySameFace(value, faceEncoding):
+        #     print(key)
 
-    # print("Leo and Leo")
-    # print(face_recognition.compare_faces([faceEncoding_CR7], faceEncoding))
-    # print("Leo and Cristiano")
-    # print(face_recognition.compare_faces([faceEncoding_CR7], faceEncoding_CR7))
-    # print("Leo and Gareth")
-    # print(face_recognition.compare_faces([faceEncoding_CR7], faceEncoding_GB))
+    # with open("known_People.json", 'r') as read_file:
+    #     data = json.load(read_file)
 
-
-
+    # print("Data: ", data)
+    printKnownPeople()
+    saveNewFaces()
